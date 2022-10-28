64965dccc9094f7ca72b0ac12347eda208fd8673
Priyanka <priyankamalekar1109gmail.com>
2022-10-28 16:30:26 +0530

    second commit

---
+++ app/src/main/java/com/cdpindia/thebhangarwalatest/ui/Activity/AppIntroActivity.kt
@@ -0,0 +1,12 @@
+package com.cdpindia.thebhangarwalatest.ui.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cdpindia.thebhangarwalatest.R
+
+class AppIntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_intro)
+    }
+}
--- app/src/main/java/com/cdpindia/thebhangarwalatest/ui/Activity/MainActivity.kt
+++ app/src/main/java/com/cdpindia/thebhangarwalatest/ui/Activity/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.cdpindia.thebhangarwalatest
+package com.cdpindia.thebhangarwalatest.ui.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cdpindia.thebhangarwalatest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 }
